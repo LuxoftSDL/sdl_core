acb48eca2d029074a08a9e158df0a366c4898b14
Andriy Byzhynar (GitHub) <AByzhynar@luxoft.com>
2020-05-20 14:04:40 +0300

    Add WEB_VIEW layout initial setup and check during windows creation

--- src/components/application_manager/rpc_plugins/sdl_rpc_plugin/src/commands/mobile/register_app_interface_request.cc
+++ src/components/application_manager/rpc_plugins/sdl_rpc_plugin/src/commands/mobile/register_app_interface_request.cc
@@ -486,6 +486,12 @@ void RegisterAppInterfaceRequest::Run() {
         }
         case mobile_apis::AppHMIType::WEB_VIEW: {
           application->set_webengine_projection_enabled(true);
+          const std::string default_webview_layout =
+              application_manager_.ScreenPredefinedLayoutToString(
+                  mobile_apis::PredefinedLayout::WEB_VIEW);
+          application->set_window_layout(
+              mobile_apis::PredefinedWindows::DEFAULT_WINDOW,
+              default_webview_layout);
           break;
         }
         default: {}
