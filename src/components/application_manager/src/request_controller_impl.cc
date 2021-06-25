f93a2dba514e8638c1708878f0dab61bd9699e2c
Vadym Luchko (gitHub) <vluchko@luxoft.com>
2021-06-25 04:13:48 +0300

    Do not manage requests with NULL timeout

--- src/components/application_manager/src/request_controller_impl.cc
+++ src/components/application_manager/src/request_controller_impl.cc
@@ -388,13 +388,20 @@ void RequestControllerImpl::UpdateRequestTimeout(const uint32_t app_id,
   SDL_LOG_DEBUG("app_id : " << app_id
                             << " mobile_correlation_id : " << correlation_id
                             << " new_timeout : " << new_timeout);
-  SDL_LOG_DEBUG(
-      "New_timeout is NULL. RequestCtrl will "
-      "not manage this request any more");
+
+  if (new_timeout == 0) {
+    SDL_LOG_DEBUG(
+        "New_timeout is NULL. RequestCtrl will "
+        "not manage this request any more");
+  }
 
   RequestInfoPtr request_info =
       waiting_for_response_.Find(app_id, correlation_id);
   if (request_info) {
+    if (0 == request_info->timeout_msec()) {
+      return;
+    }
+
     waiting_for_response_.RemoveRequest(request_info);
     request_info->updateTimeOut(new_timeout);
     waiting_for_response_.Add(request_info);
